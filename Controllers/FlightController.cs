acb202944ad0f79f7f740192bceebb302718106c
Pat <patricio.dilet@gmail.com>
2024-07-02 17:30:20 -0400

    l

---
+++ Controllers/FlightController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Flights.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FlightController : ControllerBase
+{
+    private readonly ILogger<FlightController> _logger;
+
+    public FlightController(ILogger<FlightController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<string> Search()
+        => new string[]{
+            "American Airlines",
+            "Lan",
+            "Brithish Airline"
+        };
+
+}
